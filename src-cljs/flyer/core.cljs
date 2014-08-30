1dc9e0cf27eaf749dadcebe5f37b1776a499a61d
Benjamin Zaporzan <btzaporz@lakeheadu.ca>
2014-08-30 16:16:28 -0400

    subscriptions can now be routed correctly

--- src-cljs/flyer/core.cljs
+++ src-cljs/flyer/core.cljs
@@ -1,13 +1,28 @@
 (ns flyer.core
   (:use [flyer.messaging
-         :only [publish subscribe]]))
+         :only [broadcast subscribe]]))
 
 (defn ^export main []
-  (subscribe :callback
-             (fn [data]
-               (.log js/console "I am called from parent!"))))
+  (subscribe 
+   :callback
+   (fn [data]
+     (.log js/console "I am called from parent!"))))
 
 (defn ^export child []
-  (subscribe :callback
-             (fn [data]
-               (.log js/console "I am called from child!"))))
+  (subscribe 
+   :topic "general"
+   :callback
+   (fn [data]
+     (.log js/console "I am called from child!")
+     (.log js/console "data: " data)
+     (.log js/console "data: " data)
+     (.log js/console "data: " data))))
+
+(if-let [button (.getElementById js/document "main-button")]
+  (do 
+    (.log js/console "button found!")
+    (.addEventListener 
+     button "click"
+     (fn [event]
+       (broadcast :topic "general")
+       (.log js/console "Button Pressed!")))))
--- src-cljs/flyer/messaging.cljs
+++ src-cljs/flyer/messaging.cljs
@@ -1,28 +1,38 @@
 (ns flyer.messaging
   (:require [flyer.traversal :as traversal]))
 
-(def default-message {:data nil
-                      :channel "default"
-                      :topic "*"})
+(def default-message 
+  "default message structure"
+  {:data nil
+   :topic "*"
+   :channel "*"})
 
-(def default-window js/window)
+(def default-window 
+  "the main window needs to be localized, so that it can be
+  referenced"
+  js/window)
 
-(defn default-callback [msg]
-  (.log js/console "callback:" msg))
+(defn default-callback 
+  "default callback for testing"
+  [data topic channel]
+  (.log js/console "callback-data:" data)
+  (.log js/console "callback-topic:" topic)
+  (.log js/console "callback-channel:" channel))
 
 (defn ^export window-post-message
   "performs the window postback"
   [window msg]
   (let [data-js (clj->js msg)
         data-json (.stringify js/JSON data-js)]
+    ;;TODO: this should include a good origin
     (.postMessage window data-json "*")))
 
 (defn ^export broadcast
   "broadcast message to currently active frames"
-  [{:keys [data channel topic]
-    :or {data (:data default-message)
-         channel (:channel default-message)
-         topic (:topic default-message)}}]
+  [& {:keys [data channel topic]
+      :or {data (:data default-message)
+           channel (:channel default-message)
+           topic (:topic default-message)}}]
   (let [msg {:data data :channel channel :topic topic}
         msg-js (clj->js msg)
         broadcast-list (traversal/generate-broadcast-list)]
@@ -34,7 +44,26 @@
   message postback"
   ([window callback]
      (.addEventListener window "message" callback true))
-  ([callback] (create-broadcast-listener js/window callback)))
+  ([callback] (create-broadcast-listener default-window callback)))
+
+(defn like-this-channel? 
+  [msg-channel callback-channel]
+  (some true? 
+        [(= callback-channel (default-message :channel))
+         (= msg-channel callback-channel)]))
+
+(defn like-this-topic?
+  [msg-topic callback-topic]
+  (some true?
+        [(= callback-topic (default-message :topic))
+         (= msg-topic callback-topic)]))
+
+(defn like-this-flyer?
+  "determines if the callback should be called based on the channel and the topic"
+  [msg-topic msg-channel
+   callback-topic callback-channel]
+  (every? true? [(like-this-channel? msg-channel callback-channel)
+                 (like-this-topic? msg-topic callback-topic)]))
 
 (defn ^export subscribe
   "subscribe to broadcast messages"
@@ -48,6 +77,12 @@
         (fn [event]
           (let [data (.-data event)
                 msg-js (.parse js/JSON data)
-                msg (js->clj msg-js)]
-            (callback (.-data msg-js))))]
+                msg (js->clj msg-js)
+                ;;extract data from channel
+                msg-data (.-data msg-js)
+                msg-topic (.-topic msg-js)
+                msg-channel (.-channel msg-js)]
+            (when (like-this-flyer? msg-topic msg-channel
+                                    topic channel)
+              (callback msg-data msg-topic msg-channel))))]
     (create-broadcast-listener js/window callback-wrapper)))
