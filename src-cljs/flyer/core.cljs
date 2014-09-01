930ed662d63262d940c18a306ef563d936e727c1
Benjamin Zaporzan <benzaporzan@gmail.com>
2014-09-01 14:03:53 -0400

    did some cleanup, re-structuring

--- src-cljs/flyer/core.cljs
+++ src-cljs/flyer/core.cljs
@@ -1,8 +1,14 @@
 (ns flyer.core
   (:use [flyer.window
-         :only [register-external]]))
+         :only [register-external]])
+  (:require [flyer.storage :as s]
+            [flyer.utils :as utils]))
 
 ;;if the window is external, we should re-register it for cases where
 ;;the window gets refreshed
 (when (not (nil? (.-opener js/window)))
   (register-external))
+
+;;if i'm the parent window, intiialize ref variable
+(when (= (utils/get-main-parent) js/window)
+  (aset s/storage s/window-list-key #{}))
--- src-cljs/flyer/storage.cljs
+++ src-cljs/flyer/storage.cljs
@@ -17,8 +17,3 @@
 (defn remove-window-ref! [window]
   (aset storage window-list-key
         (disj (get-window-refs) window)))
-
-;;if i'm the parent window, intiialize ref variable
-(when (= (utils/get-main-parent) js/window)
-  (.log js/console "Flyer is initializing External Window Reference")
-  (aset storage window-list-key #{}))
